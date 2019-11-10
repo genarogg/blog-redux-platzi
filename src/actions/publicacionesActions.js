610ff0028b37bfc06e93ad6dea8ed470298771c4
genarogg <gena_gonzalez15@hotmail.com>
2019-11-09 22:01:41 -0400

    others dispatch

--- src/actions/publicacionesActions.js
+++ src/actions/publicacionesActions.js
@@ -4,9 +4,12 @@ import {
 	ERROR,
 	TRAER_POR_USUARIO
 } from '../types/publicacionesTypes';
+import * as usuariosTypes from '../types/usuariosTypes';
+
+const { TRAER_TODOS: USUARIOS_TRAER_TODOS } = usuariosTypes;
 
 export const traerPorUsuario = (key) => async (dispatch, getState) => {
-	const { usuarios } = getState().usuariosReducer;
+	let { usuarios } = getState().usuariosReducer;
 	const { publicaciones } = getState().publicacionesReducer;
 	const usuario_id = usuarios[key].id;
 
@@ -15,6 +18,17 @@ export const traerPorUsuario = (key) => async (dispatch, getState) => {
 		...publicaciones,
 		respuesta.data
 	];
+	const publicaciones_key = publicaciones_actualizadas.length - 1;
+	const usuarios_actualizados = [...usuarios];
+	usuarios_actualizados[key] = {
+		...usuarios[key],
+		publicaciones_key
+	}
+
+	dispatch({
+		type: USUARIOS_TRAER_TODOS,
+		payload: usuarios_actualizados
+	});
 
 	dispatch({
 		type: TRAER_POR_USUARIO,
