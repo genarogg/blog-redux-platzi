cb4082080c0de61b7eeba7f47c42470c7a1ce09a
genarogg <gena_gonzalez15@hotmail.com>
2019-11-15 22:35:47 -0400

    coments

---
+++ src/components/Publicaciones/Comentarios.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const Comentarios = (props) => {
+	return (
+		<ul>
+			<li>
+				hola
+			</li>
+			<li>
+				hola
+			</li>
+			<li>
+				hola
+			</li>
+			<li>
+				hola
+			</li>
+			<li>
+				hola
+			</li>
+		</ul>
+	);
+};
+
+export default Comentarios;
