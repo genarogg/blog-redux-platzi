1c5fcf810059d487f6dc8491f1bad5e1232a9240
genarogg <gena_gonzalez15@hotmail.com>
2019-11-20 22:48:39 -0400

    new variables

--- src/types/usuariosTypes.js
+++ src/types/usuariosTypes.js
@@ -1,3 +1,6 @@
-export const TRAER_TODOS = 'usuarios_traer_todos';
-export const CARGANDO = 'usuarios_cargando';
-export const ERROR = 'usuarios_error';
+export const CARGANDO = 'publicaciones_cargando';
+export const ERROR = 'publicaciones_error';
+export const ACTUALIZAR = 'publicaciones_actualizar';
+export const COM_CARGANDO = 'comentarios_cargando';
+export const COM_ERROR = 'comentarios_error';
+export const COM_ACTUALIZAR = 'comentarios_actualizar';
